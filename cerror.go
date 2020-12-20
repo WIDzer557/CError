39b141e28f0c28ff90da89b294be59e20934e261
Nameless Name <62650188+WIDzer557@users.noreply.github.com>
2020-12-20 11:39:10 +0100

    Update cerror.go

--- cerror.go
+++ cerror.go
@@ -3,16 +3,16 @@ package cerror
 type EBuffer []interface{}
 
 type Catcher interface {
-	Catch(func(interface{}))
+	Catch(func(err interface{}))
 }
 
 func Throw(err ...interface{}) Catcher {
-	buff := EBuffer(err)
-	return &buff
+	buffer := EBuffer(err)
+	return &buffer
 }
 
-func (buff *EBuffer) Catch(fn func(err interface{})) {
-	for _, err := range *buff {
-		fn(err)
+func (buffer *EBuffer) Catch(catcher func(err interface{})) {
+	for _, err := range *buffer {
+		catcher(err)
 	}
 }
